92c307790d983496a0bdfad91b87903315f92cdd
XandaoPe <73856144+XandaoPe@users.noreply.github.com>
2024-02-26 15:12:26 -0300

    gitIgnore com env.ts

---
+++ configs.ts
@@ -0,0 +1,9 @@
+////config.ts
+const DATABASE_URL: string = `${process.env.DATABASE_URL}`;
+ 
+const PORT: number = parseInt(`${process.env.PORT || 3000}`);
+ 
+export default {
+    DATABASE_URL,
+    PORT
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './users/user.model';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { DATABASE_URL } from 'env';
+
+console.log(DATABASE_URL)
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://mota:xela2208@motacluster.vdoop.mongodb.net/?retryWrites=true&w=majority&appName=MotaCluster'),
+    // MongooseModule.forRoot('mongodb+srv://mota:xela2208@motacluster.vdoop.mongodb.net/?retryWrites=true&w=majority&appName=MotaCluster'),
+    MongooseModule.forRoot(DATABASE_URL),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UsersController],
